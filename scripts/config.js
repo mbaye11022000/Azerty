baece52bae94e930befa7bf8de798b5ce1d173ab
mbaye11022000 <160291036+mbaye11022000@users.noreply.github.com>
2024-08-05 23:22:04 +0000

    Add files via upload

---
+++ scripts/config.js
@@ -0,0 +1,11 @@
+/*********************************************************************************
+ * 
+ * Ce fichier contient toutes les constantes nécessaires au fonctionnement du jeu.
+ * En particulier les listes de mots et de phrases proposés à l'utilisateur
+ * 
+ *********************************************************************************/
+
+// Déclaration des tableaux contenant les listes des mots proposés à l'utilisateur
+const listeMots = ["Cachalot", "Pétunia", "Serviette" ,"Mouhamed", "Dakar", "Particulier", "Constantes", "Fonctionnement", "Actuelle", "Recherche"]
+const listePhrases = ["Pas de panique !", "La vie, l'univers et le reste", "Merci pour le poisson","Je suis désolé","Quel temps fait-il ?",
+"J'aime beaucoup ce film",  "Je serai là demain.","C'est une excellente idée !", "je ne voulais pas te blesser.","Nous devons partir tôt demain matin"]
